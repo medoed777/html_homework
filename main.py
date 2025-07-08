f025b9185cb178f33be54a96b00ef18a260f6a12
Alexey <mister.skorpion4ik@mail.ru>
2025-07-08 15:14:39 +0500

    fixed

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import os
 
 # Настройки запуска
 hostName = "localhost"  # Адрес для доступа по сети
 serverPort = 8080  # Порт для доступа по сети
+
+
+content_types = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.gif': 'image/gif',
+}
+
 
 class MyServer(BaseHTTPRequestHandler):
     """
@@ -13,14 +25,35 @@ class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+        if self.path == '/':
+            self.path = '/contacts.html'
 
-        with open("contacts.html", "r", encoding="utf-8") as file:
-            content = file.read()
+        _, ext = os.path.splitext(self.path)
 
-        self.wfile.write(bytes(content, "utf-8"))
+        try:
+            with open(self.path[1:], "rb") as file:  # Убираем первый символ '/' из пути
+                content = file.read()
+
+            content_type = content_types.get(ext, 'application/octet-stream')
+
+            # Отправка успешного ответа и заголовков
+            self.send_response(200)
+            self.send_header("Content-type", content_type)
+            self.end_headers()
+
+            self.wfile.write(content)
+
+        except FileNotFoundError:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<html><body><h1>404 Not Found</h1></body></html>")
+
+        except Exception as e:
+            self.send_response(500)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(f"<html><body><h1>500 Internal Server Error</h1><p>{str(e)}</p></body></html>", "utf-8"))
 
     def do_POST(self):
         """ Метод для обработки входящих POST-запросов """
